25fb6a99e1d1243e91ea468f8852ecd2c8daadfe
lautaro <lautarorondan96@gmail.com>
2022-12-20 02:13:47 -0300

    logo

--- src/components/layout/private/Header.jsx
+++ src/components/layout/private/Header.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { Nav } from './Nav'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Nav } from './Nav';
+import LRwhite from '/LRwhite.svg';
+
 
 export const Header = () => {
   return (
     <header className="layout__navbar">
 
       <div className="navbar__header">
-        <NavLink to="/social" className="navbar__title">LRSOCIAL</NavLink>
+        <NavLink to="/social" className="navbar__title"><img src={LRwhite}/></NavLink>
       </div>
 
       <Nav></Nav>
